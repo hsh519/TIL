a403f3fc8933f86362c35ffc361200ba103de2cc
hsh_519 <suk000519@naver.com>
2023-01-03 17:33:36 +0900

    자바 언어로만 상품(상품 주문, 할인 적용) 기능 개발

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberService memberService = new MemberServiceImpl();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member findMember = memberService.findMember(memberId);
+        int discountPrice = discountPolicy.discount(findMember, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
