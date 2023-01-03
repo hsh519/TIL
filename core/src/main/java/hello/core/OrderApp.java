8a6226e36e81bae58be516f6c6121dafd0d17ba6
hsh_519 <suk000519@naver.com>
2023-01-03 17:34:07 +0900

    주문 기능 실행과 테스트

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        /* 회원 생성 */
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        /* 상품 주문 */
+        Order order = orderService.createOrder(memberId, "item", 20000);
+        System.out.println(order);
+        System.out.println("할인가 : " + order.calculatePrice());
+    }
+}
