881470ab827a67e592f0ffd80373852424fb5506
hsh_519 <suk000519@naver.com>
2023-01-03 17:32:11 +0900

    회원 기능 실행과 테스트

---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void 회원가입() {
+        // given
+        Member newMember = new Member(1L, "memberA", Grade.VIP);
+        // when
+        memberService.join(newMember);
+        Member findMember = memberService.findMember(1L);
+        // then
+        Assertions.assertThat(newMember).isEqualTo(findMember);
+    }
+}
