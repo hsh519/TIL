5f4e395309c056888db15920cf678bfa07d33735
hsh_519 <suk000519@naver.com>
2023-01-03 17:30:45 +0900

    자바 언어로만 회원(회원가입, 회원조회) 기능 개발

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+import java.util.HashMap;
+
+public class MemberServiceImpl implements MemberService {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
