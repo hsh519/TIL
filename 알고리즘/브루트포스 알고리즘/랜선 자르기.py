882c1c843f3fbbaf1e87391ef0722a4f7a4ad15a
hsh_519 <suk000519@naver.com>
2023-03-23 17:57:38 +0900

    TIL(백준 1문제 코드 수정, 1문제 풀이)

--- 알고리즘/브루트포스 알고리즘/랜선 자르기.py
+++ 알고리즘/브루트포스 알고리즘/랜선 자르기.py
@@ -4,22 +4,17 @@ import sys
 
 n, k = map(int, sys.stdin.readline().split())
 l1 = [int(sys.stdin.readline()) for _ in range(n)]
-l2 = [i for i in range(min(l1) + 1)]
-start = 0
-end = len(l2) - 1
+start, res = 1, 0
+end = max(l1)
 mid = (start + end) // 2
 
-while True:
-    count = list(map(lambda x: x//l2[mid], l1))
-    if sum(count) == k:
-        mid += 1
-    elif sum(count) < k:
-        if sum(list(map(lambda x: x//l2[mid-1], l1))) == k:
-            mid -= 1
-            break
-        end = mid - 1
-        mid = (start + end) // 2
-    elif sum(count) > k:
+while start <= end:
+    count = sum(x//mid for x in l1)
+    if count >= k:
         start = mid + 1
         mid = (start + end) // 2
+    elif count < k:
+        end = mid - 1
+        mid = (start + end) // 2
+        
 print(mid)
