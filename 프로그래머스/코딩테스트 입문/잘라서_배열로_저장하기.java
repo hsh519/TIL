7a330cc3f7e0e8ac6a2408e4871579b2f3090e73
hsh_519 <suk000519@naver.com>
2023-03-28 18:18:12 +0900

    TIL(프로그래머스 6문제 풀이)

---
+++ 프로그래머스/코딩테스트 입문/잘라서_배열로_저장하기.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class 잘라서_배열로_저장하기 {
+    class Solution {
+        public ArrayList<String> solution(String my_str, int n) {
+            ArrayList<String> answer = new ArrayList<>();
+            while (true) {
+                String s = my_str.substring(0, n);
+                my_str = my_str.substring(n);
+                answer.add(s);
+                if (my_str.equals("")) {
+                    break;
+                }
+                if (my_str.length() <= n) {
+                    answer.add(my_str);
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
